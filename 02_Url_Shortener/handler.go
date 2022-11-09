04759d115a0532dd60327ab3c8df5cf70ef10f58
creamynebula <mateus.souzaaa@gmail.com>
2022-11-09 18:22:36 -0300

    updating

---
+++ 02_Url_Shortener/handler.go
@@ -0,0 +1,16 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if destination, ok := pathsToUrls[path]; ok { // se 'ok'
+			http.Redirect(w, r, destination, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
