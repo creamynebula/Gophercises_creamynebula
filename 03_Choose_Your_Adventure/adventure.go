11e1383a9d20f9072a9e1aac81a80ffc11b544b7
creamynebula <mateus.souzaaa@gmail.com>
2022-11-16 13:09:53 -0300

    refactoring, e html templating

--- 03_Choose_Your_Adventure/adventure.go
+++ 03_Choose_Your_Adventure/adventure.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -20,6 +22,33 @@ type Option struct {
 	Chapter string `json:"arc"`
 }
 
+var defaultHandlerTemplate = `<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Choose your own adventure!</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+
+</body>
+
+</html>`
+
 func main() {
 	// go run . -file=gopher.json
 	fileName := flag.String("file", "gopher.json", "o arquivo JSON com a historinha do jogo, default == gopher.json")
@@ -33,14 +62,27 @@ func main() {
 		panic(err)
 	}
 
+	story, err := JsonStory(file)
+	// se não deu erro, story tem as infos decodificadas do arquivo
+	// %+v é mais verboso que %v aparentemente, com mais detalhes
+	if err == nil {
+		fmt.Printf("decoded story1:\n\n%+v\n\n", story)
+	}
+
+}
+
+func JsonStory(file io.Reader) (Story, error) {
 	var story Story
 	decoder := json.NewDecoder(file)
 
 	if err := decoder.Decode(&story); err != nil {
 		fmt.Printf("\nerro no decoding\n")
-		panic(err)
+		return nil, err
 	}
-	// se não deu erro, story tem as infos decodificadas do arquivo
-	// %+v é mais verboso que %v aparentemente, com mais detalhes
-	fmt.Printf("decoded story1:\n\n%+v\n\n", story)
+
+	return story, nil
+}
+
+func NewHandler(s Story) http.Handler {
+
 }
